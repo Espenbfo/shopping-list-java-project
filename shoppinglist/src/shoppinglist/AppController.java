64866795e6dcf1ff4a2336340e098b4429cdddfe
Ane Larsen <anelar@stud.ntnu.no>
2020-09-08 09:05:25 +0000

    Added dynamic list of checkboxes in fxml

--- shoppinglist/src/shoppinglist/AppController.java
+++ shoppinglist/src/shoppinglist/AppController.java
@@ -1,15 +1,37 @@
 package shoppinglist;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.TilePane;
 
 public class AppController {
     
     @FXML
-    Button clickMeButton;
-
+    Button addItemButton;
     @FXML
-    void handleClickMeButtonAction() {
-        clickMeButton.setText("Thanks!");
+    TextField itemInputField;
+    @FXML
+    TilePane shoppingList;
+    @FXML 
+    Label emptyListText;
+    
+    
+
+    String itemToAdd = null;
+    
+    @FXML
+    void handleAddItemButtonClicked() {
+    	
+    	shoppingList.getChildren().remove(emptyListText);
+    	
+    	itemToAdd = itemInputField.getText();
+    	CheckBox shoppingListItem = new CheckBox(itemToAdd);
+    	shoppingListItem.setPadding(new Insets(10, 10, 10, 10));
+    	shoppingList.getChildren().add(shoppingListItem);
+       
     }
 }
