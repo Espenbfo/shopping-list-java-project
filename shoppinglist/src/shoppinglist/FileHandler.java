e754f025b1c73612c47401d6962814edd276a69a
Birk Gustav Samson Stoveland <bgstovel@stud.ntnu.no>
2020-09-09 00:25:52 +0200

    add filereader

--- shoppinglist/src/shoppinglist/FileHandler.java
+++ shoppinglist/src/shoppinglist/FileHandler.java
@@ -3,26 +3,31 @@ package shoppinglist;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.ArrayList;
+import java.io.FileReader;
 
 class FileHandler{
- /*
-    public shoppinglist readFile(String fileName){
-        BufferedReader BFR = new BufferedReader(fileName);
-        Arraylist<ShoppingElement> ShoppingElementList = new ArrayList<ShoppingElement>();
-        shoppingList out;
+
+    public ShoppingList readFile(String fileName){
+        BufferedReader BFR;
+        ArrayList<ShoppingElement> ShoppingElementList = new ArrayList<ShoppingElement>();
+        ShoppingList out;
         try{
-            BFR.open();
+            BFR = new BufferedReader(new FileReader(fileName));
+            String line = BFR.readLine();
+            String[] linearray = line.split(" ");
+            ShoppingElementList.add(new ShoppingElement(linearray[0], Double.parseDouble(linearray[1]),linearray[2]));
             BFR.close();
 
         }catch(Exception e){
             e.printStackTrace();
         }
-
+        out = new ShoppingList(ShoppingElementList);
+        return out;
     }
 
-    public boolean writeFile(shoppingList listToWrite){
-        BufferedWriter BFW = new BufferedWriter(shoppingList.getId());
+    public boolean writeFile(ShoppingList listToWrite){
+        BufferedWriter BFW = new BufferedWriter(ShoppingList.getId());
         return false;
     }
- */
+
 }
