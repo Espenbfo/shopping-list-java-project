6341092ecb826347a24839d97e2efd8fc42e6993
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2020-09-16 16:32:36 +0200

    Add Javadoc to core

--- shoppinglist/src/shoppinglist/core/ShoppingElement.java
+++ shoppinglist/src/shoppinglist/core/ShoppingElement.java
@@ -1,60 +1,113 @@
 package shoppinglist.core;
 
+/**
+ * Represents an element in a shoppingList.
+ */
+
 public class ShoppingElement {
     String name;
     MeasurementType measurementType;
     boolean shopped;
 
+    /**
+     * Initialize a ShoppingElement with default values.
+     */
     public ShoppingElement() {
         name= "";
         measurementType = new MeasurementType();
         shopped = false;
     }
 
+    /**
+     * Initialize a ShoppingElement with name, value, measurementName, and if the element not only is represented by integers.
+     */
     public ShoppingElement(String name, double value, String measurementName, boolean allowDouble) {
         this.name= name;
         measurementType = new MeasurementType(measurementName,value,allowDouble);
         this.shopped = false;
     }
-
+    /**
+     * Initialize a ShoppingElement with name, value, measurementName.
+     */
     public ShoppingElement(String name, double value, String measurementName) {
         this(name,value,measurementName,true);
     }
 
+    /**
+     * Initialize a ShoppingElement with name, a predefined MeasurementType, and if the item has been shopped.
+     */
     public ShoppingElement(String name, MeasurementType m, boolean shopped) {
         this.name= name;
         measurementType = m;
         this.shopped = shopped;
     }
 
+    /**
+     * Gets the value
+     *
+     * @return the value
+     */
     public double getValue() {
         return measurementType.getValue();
     }
 
+    /**
+     * Sets the value
+     *
+     * @param newValue the new value
+     */
     public void setValue(double newValue) {
         measurementType.setValue(newValue);
     }
 
+    /**
+     * Changes the value
+     *
+     * @param difference the number to change the value by
+     */
     public void changeValue(double difference) {
         measurementType.changeValue(difference);
     }
 
+    /**
+     * Toggles if the element is shopped
+     */
     public void toggleShopped() {
         shopped = !shopped;
     }
 
+    /**
+     * Gets whether the element is shopped or not
+     *
+     * @return shopped
+     */
     public boolean isShopped() {
         return shopped;
     }
 
+    /**
+     * Gets the name
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
-    public void getName(String newName) {
+    /**
+     * Sets the name
+     *
+     * @param newName the new name
+     */
+    public void setName(String newName) {
         name = newName;
     }
 
+    /**
+     * Gets a description of the element
+     *
+     * @return a String representation of the element
+     */
     @Override
     public String toString() {
         return "name='" + name + '\'' + "\n" +
--- shoppinglist/src/shoppinglist/core/ShoppingList.java
+++ shoppinglist/src/shoppinglist/core/ShoppingList.java
@@ -2,6 +2,11 @@ package shoppinglist.core;
 
 import java.util.ArrayList;
 import java.lang.Math;
+
+/**
+ * Represents a list of ShoppingElements.
+ */
+
 public class ShoppingList {
     String title;
     int id;
@@ -9,6 +14,9 @@ public class ShoppingList {
     ArrayList<Person> personList;
     ArrayList<ShoppingElement> elementList;
 
+    /**
+     * Initialize a ShoppingList with default values.
+     */
     public ShoppingList() {
         currentMaxID += 1;
         id = currentMaxID;
@@ -16,12 +24,25 @@ public class ShoppingList {
         this.personList = new ArrayList<Person>();
         this.title = "";
     }
-    
+
+    /**
+     * Initialize a ShoppingList with a title.
+     *
+     * @param title the title
+     */
     public ShoppingList(String title) {
         this();
         this.title = title;
     }
-    
+
+    /**
+     * Initialize a ShoppingList with a title, id, personList, and elementList.
+     *
+     * @param title the title
+     * @param id the ID
+     * @param personList the list of people
+     * @param elementList the list of elements
+     */
     public ShoppingList(String title, int id, ArrayList<Person> personList, ArrayList<ShoppingElement> elementList) {
         this.title = title;
         this.id = id;
@@ -30,50 +51,105 @@ public class ShoppingList {
         currentMaxID = Math.max(currentMaxID,id);
     }
 
+    /**
+     * Gets the title
+     * @return the Title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title
+     * @param newTitle the new title
+     */
     public void setTitle(String newTitle) {
         title = newTitle;
     }
 
+    /**
+     * Gets the id
+     * @return the Id
+     */
     public int getId() {
         return id;
     }
 
-
+    /**
+     * Gets the list of elements
+     * @return the elementList
+     */
     public ArrayList<ShoppingElement> getElementList() {
         return elementList;
     }
 
+    /**
+     * Gets the element at an index
+     * @param index the index
+     * @return the element at the specified index
+     */
     public ShoppingElement getElement(int index) {
         return elementList.get(index);
     }
 
+    /**
+     * Adds an element to the end of the elementList
+     * @param e the element to be added
+     */
     public void addElement(ShoppingElement e) {
         elementList.add(e);
     }
 
+    /**
+     * Adds a new element with the specified values
+     * @param name the name
+     * @param value the value
+     * @param measurementName the measurement name
+     * @param allowDouble whether the element can be represented by non-integers
+     */
     public void addElement(String name, double value, String measurementName,boolean allowDouble) {
         elementList.add(new ShoppingElement(name,value, measurementName,allowDouble));
     }
+
+    /**
+     * Adds a new element with the specified values
+     * @param name the name
+     * @param value the value
+     * @param measurementName the measurement name
+     */
     public void addElement(String name, double value, String measurementName) {
         elementList.add(new ShoppingElement(name,value, measurementName,true));
     }
 
+    /**
+     * Removes the element at the specified index
+     * @param index the index
+     */
     public void removeElement(int index) {
         elementList.remove(index);
     }
 
+    /**
+     * Removes the specified element
+     * @param e the element to be removed
+     */
     public void removeElement(ShoppingElement e) {
         elementList.remove(e);
     }
 
+    /**
+     * Gets the personList
+     * @return the personList
+     */
     public ArrayList<Person> getPersonList() {
         return personList;
     }
 
+    /**
+     * Gets a description of the list, and all the elements in it
+     *
+     * @return a String representation of the list
+     */
     public String toString() {
         String elementString = "";
         for (ShoppingElement e : getElementList()) {
