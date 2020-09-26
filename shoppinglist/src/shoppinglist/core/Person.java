539b96c7e8ed3ed5be3cb7c4a48e67168dcf57ff
espenbfo <espenbfo@stud.ntnu.no>
2020-09-26 12:28:07 +0200

    Update Person with equals() method and related methods.

--- shoppinglist/src/shoppinglist/core/Person.java
+++ shoppinglist/src/shoppinglist/core/Person.java
@@ -3,7 +3,66 @@ package shoppinglist.core;
 import java.util.ArrayList;
 
 public class Person {
-    String userName;
-    int id;
-    ArrayList<ShoppingList> shoppingLists;
+    private String userName;
+    private ArrayList<ShoppingList> shoppingLists;
+
+
+    /**
+     * Gets the username
+     * @return the username
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets the username
+     * @param newName new name
+     */
+    public void setUserName(String newName) {
+        userName = newName;
+    }
+
+    /**
+     * Gets the shoppinglist at an index
+     * @param index the index
+     * @return the shoppinlist at the specified index
+     */
+    public ShoppingList getShoppingList(int index) {
+        return shoppingLists.get(index);
+    }
+
+
+    public ArrayList<ShoppingList> getShoppingLists() {
+        return shoppingLists;
+    }
+
+    /**
+     * Checks if instance equals another
+     *
+     * @return true if equal, false else
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (!o.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        Person p = (Person) o;
+
+        if (!getUserName().equals(p.getUserName())) {
+            return false;
+        }
+
+        if (getShoppingLists().size() != p.getShoppingLists().size()) {
+            return false;
+        }
+
+        for (int i = 0; i < getShoppingLists().size(); i++) {
+            if (!getShoppingList(i).equals(p.getShoppingList(i))) return false;
+        }
+
+
+        return true;
+    }
 }
