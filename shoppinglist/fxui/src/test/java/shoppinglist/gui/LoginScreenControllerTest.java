acec6f8d0643a81488f226d040bdd8f80e9170bc
Ane Larsen <anelar@stud.ntnu.no>
2020-10-11 14:30:42 +0000

    Create login test

---
+++ shoppinglist/fxui/src/test/java/shoppinglist/gui/LoginScreenControllerTest.java
@@ -0,0 +1,49 @@
+package shoppinglist.gui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import shoppinglist.gui.AppController;
+import shoppinglist.gui.LoginScreenController;
+import shoppinglist.core.ShoppingList;
+import shoppinglist.core.Person;
+import shoppinglist.core.Client;
+
+import java.awt.*;
+
+public class LoginScreenControllerTest extends ApplicationTest {
+
+    private Parent parent;
+    private LoginScreenController controller;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/shoppinglist/gui/App.fxml"));
+        parent = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        Scene scene = new Scene(parent);
+        scene.getStylesheets().add(getClass().getResource("/resources/shoppinglist/gui/style.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    
+    @Test
+    public void testLogIn() {
+        final Button loginButton = (Button) parent.lookup("#loginButton");
+        final TextField usernameField = (TextField) parent.lookup("#usernameField");
+        clickOn(usernameField);
+        write("Gud");
+        clickOn(loginButton);
+        Assertions.assertTrue(Client.getCurrentPerson().getUserName().equals("Gud"));
+    }
+    
+}
