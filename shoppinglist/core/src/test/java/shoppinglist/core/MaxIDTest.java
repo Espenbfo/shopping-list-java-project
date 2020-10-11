31d5b2784f8a7496a2529e9cb25d077c481fb2c2
espenbfo <espenbfo@stud.ntnu.no>
2020-10-11 16:44:31 +0200

    Update PersonTest Add MaxIDTest

---
+++ shoppinglist/core/src/test/java/shoppinglist/core/MaxIDTest.java
@@ -0,0 +1,23 @@
+package shoppinglist.core;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import shoppinglist.core.MaxID;
+
+
+public class MaxIDTest{
+
+
+    @Test
+    public void MaxIDTestAll() {
+        MaxID m = new MaxID();
+        m.setId(3);
+        MaxID m2 = new MaxID(3);
+        assertTrue(m.getId() == m2.getId());
+    }
+}
