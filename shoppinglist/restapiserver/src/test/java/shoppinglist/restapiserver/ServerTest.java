e0309915892a239a0c11779f6d7b6d69ae61f997
birkstoveland <bgstovel@stud.ntnu.no>
2020-10-16 13:33:46 +0200

    Add ServerTest.java, with empty base methods for issue #44 ServerTest

---
+++ shoppinglist/restapiserver/src/test/java/shoppinglist/restapiserver/ServerTest.java
@@ -0,0 +1,66 @@
+package shoppinglist.restapiserver;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.util.Collections;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+
+import shoppinglist.core.*;
+
+
+public class ServerTest extends JerseyTest{
+
+
+    @Override
+    protected ResourceConfig configure() {
+        final ResourceConfig config = new ResourceConfig();
+        return config;
+    }
+
+    @Override
+    protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+        return new GrizzlyTestContainerFactory();
+    }
+
+
+
+    @Test
+    public void testPost(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGet(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void testWrite(){
+        assertTrue(true);
+    }
+}
+
+
+
+
+
