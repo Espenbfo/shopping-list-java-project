589cbe6e4715e0d7ca782168fea4c56c62572496
Ane Larsen <anelar@stud.ntnu.no>
2020-10-30 13:32:46 +0000

    Added new file, moved some functionality from GrizzlyApp

---
+++ shoppinglist/restapiserver/src/main/java/shoppinglist/restapiserver/PersonConfig.java
@@ -0,0 +1,30 @@
+package simpleex.restserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import shoppinglist.core.Person;
+
+
+public class PersonConfig extends ResourceConfig {
+
+    public static Person person = new Person();
+
+    public PersonConfig() {
+
+        ResourceConfig rc = new PersonConfig();
+
+        rc.register(testPage.class);
+        rc.register(PersonService.class);
+
+        register(new AbstractBinder() {
+        @Override
+        protected void configure() {
+            bind(person);
+        }
+      });
+
+  }
+}
