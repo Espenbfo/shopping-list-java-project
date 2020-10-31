893d908abbdc6eecb1fdc5333ab95c6d3ef88bce
Ane Larsen <anelar@stud.ntnu.no>
2020-10-31 14:47:20 +0000

    Move some functionality to App class

--- shoppinglist/restapiserver/src/main/java/shoppinglist/restapiserver/PersonConfig.java
+++ shoppinglist/restapiserver/src/main/java/shoppinglist/restapiserver/PersonConfig.java
@@ -5,24 +5,23 @@ import java.io.IOException;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import java.util.ArrayList;
 import shoppinglist.core.Person;
 import shoppinglist.restapi.PersonService;
 
 public class PersonConfig extends ResourceConfig {
 
-    public static Person person = new Person();
+    public static  ArrayList<Person> persons = new ArrayList<>();
 
     public PersonConfig() {
 
-        ResourceConfig rc = new ResourceConfig();
-
-        rc.register(testPage.class);
-        rc.register(PersonService.class);
+        register(testPage.class);
+        register(PersonService.class);
 
         register(new AbstractBinder() {
         @Override
         protected void configure() {
-            bind(person);
+            bind(persons);
         }
       });
 
