b13ecb1c0574014c97af378efda718ab349edde0
espenbfo <espenbfo@stud.ntnu.no>
2020-11-02 12:16:20 +0100

    Update ShoppingListTest add tests for PersonList

--- shoppinglist/core/src/test/java/shoppinglist/core/ShoppingListTest.java
+++ shoppinglist/core/src/test/java/shoppinglist/core/ShoppingListTest.java
@@ -6,18 +6,17 @@ import shoppinglist.core.ShoppingList;
 /*import shoppinglist.gui.AppController;*/
 
 
-public class ShoppingListTest{
-
+public class ShoppingListTest {
 
 
     @Test
     public void testShoppingList() {
         ShoppingList l2 = new ShoppingList("Liste 2");
-        l2.addElement("Sparkesykkel",1.0,"stk",false);
+        l2.addElement("Sparkesykkel", 1.0, "stk", false);
 
         Assertions.assertTrue(l2.getElement(0).getValue() == 1.0);
 
-        l2.addElement("ballonger",10.0,"stk",false);
+        l2.addElement("ballonger", 10.0, "stk", false);
         l2.getElement(1).setValue(4.0);
 
         Assertions.assertFalse(l2.getElement(1).isShopped());
@@ -25,5 +24,15 @@ public class ShoppingListTest{
 
         Assertions.assertTrue(l2.getElement(1).getValue() == 4.0);
         Assertions.assertTrue(l2.getElement(1).isShopped());
+    }
+
+    @Test
+    public void testPerson() {
+        ShoppingList l2 = new ShoppingList("Liste 2");
+        l2.addPerson(new Person("Per"));
+        l2.addPerson("Per");
+        l2.addPerson("Lise");
+        Assertions.assertTrue(l2.getPersonList().size() == 2);
+        Assertions.assertTrue(l2.getPersonList().contains("Lise"));
     }
 }
