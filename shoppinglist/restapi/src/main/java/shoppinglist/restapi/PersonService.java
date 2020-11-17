4b53134906023b0b7898f63b10200ee5ea9a7c32
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2020-11-17 15:17:34 +0100

    Update PersonService add more comments

--- shoppinglist/restapi/src/main/java/shoppinglist/restapi/PersonService.java
+++ shoppinglist/restapi/src/main/java/shoppinglist/restapi/PersonService.java
@@ -105,11 +105,21 @@ public class PersonService {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public int addShoppingList(final ShoppingList shoppinglist) {
+    //Gets the id of the consumed shoppinglist
     int newId = shoppinglist.getId();
+
+    //If the newID is -1, it means the clint sent in a completely new list
     if (newId == -1) {
+      //Gets the current max ID
       newId = FileHandler.readMaxId();
+
+      //Increase it by 1 to ensure the new list has a unique id
       newId++;
+
+      //Set the new id to the list
       shoppinglist.setId(newId);
+
+      //Write the new max ID to file
       FileHandler.writeMaxId(newId);
     } else {
       //The list prevously saved to file.
@@ -122,32 +132,52 @@ public class PersonService {
         //The users that are no longer on the list must be removed.
         List<String> toBeRemoved = new ArrayList<String>();
 
+        //Find the users that no longer are on the list
         for (String p : oldList.getPersonList()) {
-          System.out.println(p);
           try {
             if (!peopleNames.contains(p)) {
+              //Reads the person
               Person person = FileHandler.readPerson(p);
+
+              //Removes the shoppinglist, as they no longer are guest owners of it
               person.removeShoppingListById(newId);
+
+              //Save the person to file
               FileHandler.writePerson(person);
+
+              //Add them to be removed from the shoppinglist later.
               toBeRemoved.add(person.getUserName());
             }
           } catch (Exception ex) {
             System.out.println(ex);
           }
         }
+
+        //remove all users in toBeRemoved from the shoppingList
         for (String username : toBeRemoved) {
           shoppinglist.removePerson(username);
         }
       }
     }
+
+    //Adds the shoppinglist to all new guest owners
     for (String x : shoppinglist.getPersonList()) {
       Person aperson = FileHandler.readPerson(x);
+
+      //Checks if the person exists and is not already a part of the list.
       if (aperson != null && !aperson.getShoppingLists().contains(newId)) {
+        //Add the shoppinglist
         aperson.addShoppingList(newId);
+
+        //Save the person
         FileHandler.writePerson(aperson);
       }
     }
+
+    //Save the shoppinglist
     FileHandler.writeFile(shoppinglist);
+
+    //returns the new ID of the shoppinglist
     return newId;
   }
 
